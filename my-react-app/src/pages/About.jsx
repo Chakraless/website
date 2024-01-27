e97c12dcfb57b0006fad21ec1d90991b36f230ca
Chakraless <zachiinferno1@gmail.com>
2024-01-26 21:23:59 -0500

    3rd commit

--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './About.css'
+import './About.css';
+
 export default function About() {
   const h1Style = {
     textAlign: "center",
@@ -9,10 +10,25 @@ export default function About() {
   return (
     <div>
       <h1 style={h1Style}>About Me</h1>
-      <div classname="card">
-        <img src="https://i.stack.imgur.com/1ruFA.jpg?s=256&g=1"></img>
+      <div className="card">
+        <img src="https://i.stack.imgur.com/1ruFA.jpg?s=256&g=1" className="card-image" alt="profile-pic" />
         <h2>Chakraless</h2>
-        <p>I like to code</p>
+        <p>Future Front-End Web Developer</p>
+        
+        {/* Social links section */}
+        <div className="social-links">
+          {/* GitHub link */}
+          <a href="https://github.com/Chakraless" target="_blank" rel="noopener noreferrer">
+            <img src="src\assets\github-logo.png" alt="GitHub" />
+          </a>
+          
+          {/* Discord link */}
+          <a href="https://discordapp.com/users/853613812313620481" target="_blank" rel="noopener noreferrer">
+            <img src="src\assets\discord.png" alt="Discord" />
+          </a>
+          
+          {/* Add more social links as needed */}
+        </div>
       </div>
     </div>
   );

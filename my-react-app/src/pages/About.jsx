67ea76153bb13d98d99a1b9b33246bb7ddcb5445
Chakraless <zachiinferno1@gmail.com>
2024-01-27 14:21:25 -0500

    eh

--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -1,35 +1,72 @@
 import React from "react";
-import './About.css';
+import "./About.css";
 
 export default function About() {
   const h1Style = {
     textAlign: "center",
-    // Add any other styles you need
+    fontWeight: "bold",
   };
 
+  const h2Style = {
+    textAlign: "center",
+    fontWeight: "bold",
+  }
+
   return (
-    <div>
-      <h1 style={h1Style}>About Me</h1>
+    <>
+    <div className="card-container">
       <div className="card">
-        <img src="https://i.stack.imgur.com/1ruFA.jpg?s=256&g=1" className="card-image" alt="profile-pic" />
-        <h2>Chakraless</h2>
-        <p>Future Front-End Web Developer</p>
-        
+        <img
+          src="https://i.stack.imgur.com/1ruFA.jpg?s=256&g=1"
+          className="card-image"
+          alt="profile-pic"
+        />
+        <h1>Chakraless</h1>
+        <h3>Future Front-End Web Developer</h3>
+
         {/* Social links section */}
         <div className="social-links">
           {/* GitHub link */}
-          <a href="https://github.com/Chakraless" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/Chakraless"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src="src\assets\github-logo.png" alt="GitHub" />
           </a>
-          
+
           {/* Discord link */}
-          <a href="https://discordapp.com/users/853613812313620481" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://discordapp.com/users/853613812313620481"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src="src\assets\discord.png" alt="Discord" />
           </a>
-          
+
+          {/* LinkedIn Link */}
+          <a
+            href="https://discordapp.com/users/853613812313620481"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png" alt="Discord" />
+          </a>
           {/* Add more social links as needed */}
         </div>
       </div>
+      
     </div>
+    <div className="language-container">
+      <div className="languages">
+      <h2 style={h2Style}>My Languages</h2>
+      <p></p>
+
+      <div className="image-container">
+        <img className="image"></img>
+      </div>
+      </div>
+    </div>
+  </>
   );
 }

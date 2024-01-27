4665f75303da47668667411f96dfd5063820e834
Chakraless <zachiinferno1@gmail.com>
2024-01-26 20:49:12 -0500

    FIRST COMMIT

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -5,7 +5,7 @@ function Card(){
     return(
         <div className="card">
             <img className="card-image" src={ProfilePic} alt="profile picture" width="96" height="96"></img>
-            <h2 className="card-title">Chakraless</h2>
+            <h2 className="card-title">John Doe</h2>
             <p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident officia temporibus culpa. Aliquid, asperiores, tempora consequatur atque minima at, vel in autem optio recusandae doloribus repudiandae nesciunt odit architecto ea.</p>
         </div>
     );
